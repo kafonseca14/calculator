d70c1b300996fa7af3e5982f804d26fb028ccc12
kafonseca14 <karoline.fonsecca@outlook.com>
2024-03-14 14:36:45 -0300

    Script Done

--- script.js
+++ script.js
@@ -7,12 +7,40 @@ const keyNumbers = Array.from(document.querySelectorAll(".numbers"));
 //Declaração do botões operadores 
 const keyOperators = Array.from(document.querySelectorAll(".operators"));
 
+//Declaração do botão de igual
+const equalSign = document.querySelector('#equal-sign');
+
+//Declaração de variáveis para guardar valores para soma
 let newNumber = true;
 let operator;
 let oldNumber;
 
+//Função do cálculo
+const calculate = () => {
+
+    let actualNumber = parseFloat(display.textContent)
+    newNumber = true
+
+    switch (operator) {
+        case '+':
+            updateDisplay(oldNumber + actualNumber);
+            break;
+        case '-':
+            updateDisplay(oldNumber - actualNumber);
+            break;
+        case 'x':
+            updateDisplay(oldNumber * actualNumber);
+            break;
+        case '÷':
+            updateDisplay(oldNumber / actualNumber);
+            break;
+    }
+}
+
+//Função que recebe o valor captado no evento para mostrar na tela
 const updateDisplay = (number) => {
-    if (newNumber){
+    //Verificação se é um novo número (que não seja zero)
+    if (newNumber) {
         display.textContent = number
         newNumber = false
     } else {
@@ -20,28 +48,37 @@ const updateDisplay = (number) => {
     }
 };
 
+//Função que recebe o evento 'click' como parâmetro e chama outra função passando o valor captado no evento
 const insertNumber = (event) => {
     updateDisplay(event.target.value);
 };
 
-keyNumbers.forEach (number => {
+//Evento de click para as teclas numéricas
+keyNumbers.forEach(number => {
     number.addEventListener('click', insertNumber)
 });
 
+//Função para armazenar alguns valores para o cálculo
 const selectOperator = (event) => {
     if (!newNumber) {
         newNumber = true;
         operator = event.target.textContent;
-        oldNumber = display.textContent;
+        oldNumber = parseFloat(display.textContent);
     }
-}
+};
 
-keyOperators.forEach (operator => {
+//Evento de click para as teclas operadores
+keyOperators.forEach(operator => {
     operator.addEventListener('click', selectOperator)
-});
+})
 
+//Evento de click para a tecla igual
+equalSign.addEventListener('click', calculate)
+
+//Declaração da tecla AC e Função para limpar o dígito da tela
 const clear = document.querySelector("#clear")
-clear.addEventListener('click', function() {
+clear.addEventListener('click', function () {
     display.innerHTML = '0'
+    newNumber = true
 });
 
